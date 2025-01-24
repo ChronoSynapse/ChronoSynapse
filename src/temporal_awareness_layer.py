5df6e1d36920846914a66132f6851f1bed1f4193
ChronoSynapse <portico_puffier.3l@icloud.com>
2025-01-24 15:41:50 +0530

    cleanup and spacing adjustments across source code files

--- src/temporal_awareness_layer.py
+++ src/temporal_awareness_layer.py
@@ -16,3 +16,4 @@ class TemporalAwarenessLayer:
         """ Predict the next time step based on the current data """
         prediction = self.model.predict([current_data[-self.time_steps:]])
         return prediction
+        
